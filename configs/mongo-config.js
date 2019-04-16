1c39a9062d6e65df5ae7d03427df2a9ed8ccdfb4
levelopers <youcancallmeallen@gmail.com>
2019-04-16 14:53:12 -0400

    Update mongo-config.js

--- configs/mongo-config.js
+++ configs/mongo-config.js
@@ -1,3 +1,3 @@
 //replace <username> and <password> with your mongoDB atlas accounts info
 
-module.exports="mongodb+srv://bob:yds2192@cluster0-ybbc8.mongodb.net/test?retryWrites=true"
+module.exports="mongodb+srv://<username>:<password>@cluster0-ybbc8.mongodb.net/test?retryWrites=true"
