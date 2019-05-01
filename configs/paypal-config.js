685a59f9d033b2f8a25517cf143036125efc43fe
Zack Yin <youcancallmeallen@gmail.com>
2019-04-30 21:57:19 -0400

    Update paypal-config.js

--- configs/paypal-config.js
+++ configs/paypal-config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'mode': 'sandbox', //sandbox or live
-  'client_id': 'AWijdE5xWwu7gyt-rkEjgvucwKrC3vTFhwY4L-FMXZAQSKJzQdJY-d0I51JJZxwFag6mCcDhTVEX-WYk',
-  'client_secret': 'EAWr7rG9XVeyK2RLLvg1tdrOzqQj1RaEVI6jIWNkrDpTTUKXfOUBtZFSDNJ-DZ_uOCQts_Kt_GqO8Xn9'
+  'client_id': '******',
+  'client_secret': '******'
 }
